111901f273990cfe1fc88a67b227bfd255c9b25c
Aashish Rajput <156011212+aashishrajput9838@users.noreply.github.com>
2025-02-04 04:41:59 +0530

    Update List.java

--- src/com/java/array/list/List.java
+++ src/com/java/array/list/List.java
@@ -6,41 +6,43 @@ import java.util.Collections;
 public class List {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> list= new ArrayList<Integer>();
+		ArrayList<Integer> list= new ArrayList<>();
 		
 		//add elements
 		list.add(0);
 		list.add(1);
 		list.add(2);
 		
-		System.out.println(list);
+		System.out.println("ORIGNAL LIST: "+ list);
 		
 		//get element
 		int element= list.get(0);
-		System.out.println(element);
+		System.out.println("ELEMENT AT INDEX 0 is: "+ element);
 		
 		//add element in between
 		list.add(3, 3);
-		System.out.println(list);
+		System.out.println("LIST AFTER INSERTING 3 AT POSITION 4 is: "+ list);
 		
 		//set element 
 		list.set(0, 5);
-		System.out.println(list);		
+		System.out.println("LIST AFTER UPDATING 1st ELEMENT TO 5 is: "+ list);		
 		
 		//remove element
 		list.remove(0);
-		System.out.println(list);
+		System.out.println("LIST AFTER REMOVING 1st ELEMENT is: "+ list);
 	
 		int  size= list.size();
-		System.out.println(size);
+		System.out.println("SIZE is: "+ size);
 		
 		//Loops
+		System.out.println("DISPLAYING LIST USING FOR LOOP");
 		for(int i=0; i<list.size(); i++) {
-			System.out.println(list.get(i));
+			System.out.print(" " +list.get(i));
 			
 		}
+		System.out.println();	
 		//sorting
 		Collections.sort(list);
-		System.out.println(list);
+		System.out.println("LIST AFTER SORTING is: "+ list);
 	}
 }
